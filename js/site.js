50f4a7b991e7585a8ecbe34de366b08c36935a53
Robert Stutzman <robert.stutzman.01@outlook.com>
2022-05-23 11:23:49 -0700

    site.js updated

---
+++ js/site.js
@@ -0,0 +1,24 @@
+//get the valus from the page
+//starts or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    alert("The Start Value is:" + startValue);
+    //we call generate numbers
+
+    //we call display numbers
+}
+
+//generate the numbers from the startValue to the endValue
+//logic function(s)
+function generateNumbers(){
+
+}
+
+//display the numbers and mark the even numbers bold
+//display or view functions
+function displayNumbers(){
+
+}
